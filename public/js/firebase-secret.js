604f2ac772f6194cac31e3b4e52da9be5b6ee5ff
Cameron Fung <Cameronfuung@gmail.com>
2022-12-19 15:15:02 -0800

    changed format of projects

---
+++ public/js/firebase-secret.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDAU_-ItIsn1V8rZY3jaTtvghkhChUMgl8",
+  authDomain: "cameron-fung.firebaseapp.com",
+  projectId: "cameron-fung",
+  storageBucket: "cameron-fung.appspot.com",
+  messagingSenderId: "530220486170",
+  appId: "1:530220486170:web:d053d84c57db571a7b4ba3",
+  measurementId: "G-XL2DD3S3EB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
